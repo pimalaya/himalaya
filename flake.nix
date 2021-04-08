8505a8dc41e4791d727e67f20d415a42a3aeb28e
Sridhar Ratnakumar <srid@srid.ca>
2021-04-08 09:03:08 -0400

    Get rid of gitignore crate
    cf. https://www.reddit.com/r/rust/comments/mmbfnj/nixifying_a_rust_project/gtsgxal/?context=3

--- flake.nix
+++ flake.nix
@@ -9,17 +9,13 @@
       url = "github:balsoft/crate2nix/tools-nix-version-comparison";
       flake = false;
     };
-    gitignore = { 
-      url = "github:hercules-ci/gitignore"; 
-      flake=false; 
-    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, gitignore, rust-overlay, crate2nix, ... }:
+  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
     utils.lib.eachDefaultSystem
       (system:
        let 
@@ -39,10 +35,9 @@
           };
           inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
             generatedCargoNix;
-          inherit (import gitignore { inherit (pkgs) lib; }) gitignoreSource;
           project = pkgs.callPackage (generatedCargoNix {
             inherit name;
-            src = gitignoreSource ./.;
+            src = ./.;
           }) {
             # Individual crate overrides go here
             # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
