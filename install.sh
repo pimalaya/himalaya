06383871d004b1a6cb153a5d1b2c0b43635d4269
Gregory Chamberlain <greg@cosine.blue>
2021-04-18 03:29:58 +0100

    Break long line

--- install.sh
+++ install.sh
@@ -24,7 +24,8 @@ tmpdir=$(mktemp -d) || die "Failed to create tmpdir"
 trap "rm -rf $tmpdir" EXIT
 
 echo "Downloading latest $system release…"
-curl -sLo "$tmpdir/himalaya.tar.gz" "$RELEASES_URL/latest/download/himalaya-$system.tar.gz"
+curl -sLo "$tmpdir/himalaya.tar.gz" \
+     "$RELEASES_URL/latest/download/himalaya-$system.tar.gz"
 
 echo "Installing binary…"
 tar -xzf "$tmpdir/himalaya.tar.gz" -C "$tmpdir"
