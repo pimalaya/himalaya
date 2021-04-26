1f1afad3c657741fa453c3dc882ab42034f749a4
Clément DOUIN <soywod@users.noreply.github.com>
2021-04-26 14:16:58 +0200

    add shrink_col_index and max_width method to Table trait

--- src/table2.rs
+++ src/table2.rs
@@ -6,9 +6,11 @@ where
 {
     fn head() -> Vec<String>;
     fn row(&self) -> Vec<String>;
+    fn shrink_col_index() -> usize;
+    fn max_width() -> usize;
     fn build(items: Vec<Self>) -> String {
         let mut table = vec![Self::head()];
-        let mut max_col_sizes: Vec<usize> = table[0]
+        let mut max_col_widths: Vec<usize> = table[0]
             .iter()
             .map(|col| UnicodeWidthStr::width(col.as_str()))
             .collect();
@@ -19,12 +21,14 @@ where
                     let row = item.row();
                     row.iter().enumerate().for_each(|(i, col)| {
                         let unicode_width = UnicodeWidthStr::width(col.as_str());
-                        max_col_sizes[i] = max_col_sizes[i].max(unicode_width);
+                        max_col_widths[i] = max_col_widths[i].max(unicode_width);
                     });
                     row
                 })
                 .collect::<Vec<_>>(),
         );
+
+        let table_width: usize = max_col_widths.iter().sum();
 
         table
             .into_iter()
@@ -32,10 +36,32 @@ where
                 row.into_iter()
                     .enumerate()
                     .map(|(i, mut col)| {
-                        let unicode_width = UnicodeWidthStr::width(col.as_str());
-                        let repeat_len = max_col_sizes[i] - unicode_width + 1;
-                        col.push_str(&" ".repeat(repeat_len));
-                        col
+                        if i == Self::shrink_col_index() && table_width > Self::max_width() {
+                            let shrink_width = table_width - Self::max_width();
+                            let max_col_width =
+                                max_col_widths[i] - shrink_width - (max_col_widths.len() * 2) + 1;
+                            if max_col_width < UnicodeWidthStr::width(col.as_str()) {
+                                let end = col
+                                    .char_indices()
+                                    .map(|(i, _)| i)
+                                    .nth(max_col_width - 1)
+                                    .unwrap();
+                                println!("end: {}", end);
+                                let mut col = col[..end].to_string();
+                                col.push_str("… ");
+                                col
+                            } else {
+                                let unicode_width = UnicodeWidthStr::width(col.as_str());
+                                let repeat_len = max_col_width - unicode_width + 1;
+                                col.push_str(&" ".repeat(repeat_len));
+                                col
+                            }
+                        } else {
+                            let unicode_width = UnicodeWidthStr::width(col.as_str());
+                            let repeat_len = max_col_widths[i] - unicode_width + 1;
+                            col.push_str(&" ".repeat(repeat_len));
+                            col
+                        }
                     })
                     .collect::<Vec<_>>()
                     .join("|")
@@ -50,36 +76,109 @@ mod tests {
     use super::Table;
 
     struct Item {
-        id: u8,
+        id: u16,
         name: String,
+        desc: String,
     }
 
     impl Item {
-        pub fn new(id: u8, name: &str) -> Self {
+        pub fn new(id: u16, name: &str, desc: &str) -> Self {
             Self {
                 id,
                 name: name.to_owned(),
+                desc: desc.to_owned(),
             }
         }
     }
 
     impl Table for Item {
         fn head() -> Vec<String> {
-            vec![String::from("ID"), String::from("NAME")]
+            vec![
+                String::from("ID"),
+                String::from("NAME"),
+                String::from("DESC"),
+            ]
         }
 
         fn row(&self) -> Vec<String> {
-            vec![self.id.to_string(), self.name.to_owned()]
+            vec![
+                self.id.to_string(),
+                self.name.to_owned(),
+                self.desc.to_owned(),
+            ]
+        }
+
+        fn shrink_col_index() -> usize {
+            1
+        }
+
+        fn max_width() -> usize {
+            20
         }
     }
 
     #[test]
-    fn table_structure() {
-        let items = vec![Item::new(1, "a"), Item::new(2, "b"), Item::new(3, "c")];
+    fn row_smaller_than_head() {
+        let items = vec![
+            Item::new(1, "a", "aa"),
+            Item::new(2, "b", "bb"),
+            Item::new(3, "c", "cc"),
+        ];
+        let table = "
+ID |NAME |DESC 
+1  |a    |aa   
+2  |b    |bb   
+3  |c    |cc   
+";
 
-        assert_eq!(
-            String::from("ID |NAME \n1  |a    \n2  |b    \n3  |c    "),
-            Table::build(items)
-        );
+        assert_eq!(String::from(table.trim_matches('\n')), Table::build(items));
+    }
+
+    #[test]
+    fn row_bigger_than_head() {
+        let items = vec![
+            Item::new(1, "a", "aa"),
+            Item::new(2222, "bbbbb", "bbbbb"),
+            Item::new(3, "c", "cc"),
+        ];
+        let table = "
+ID   |NAME  |DESC  
+1    |a     |aa    
+2222 |bbbbb |bbbbb 
+3    |c     |cc    
+";
+
+        assert_eq!(String::from(table.trim_matches('\n')), Table::build(items));
+
+        let items = vec![
+            Item::new(1, "a", "aa"),
+            Item::new(2222, "bbbbb", "bbbbb"),
+            Item::new(3, "cccccc", "cc"),
+        ];
+        let table = "
+ID   |NAME   |DESC  
+1    |a      |aa    
+2222 |bbbbb  |bbbbb 
+3    |cccccc |cc    
+";
+
+        assert_eq!(String::from(table.trim_matches('\n')), Table::build(items));
+    }
+
+    #[test]
+    fn shrink() {
+        let items = vec![
+            Item::new(1, "aaaaaaaaa", "aa"),
+            Item::new(2, "bbbbbbbbbbbbbbbbb", "bb"),
+            Item::new(3, "c", "cc"),
+        ];
+        let table = "
+ID |NAME      |DESC 
+1  |aaaaaaaaa |aa   
+2  |bbbbbbbb… |bb   
+3  |c         |cc   
+";
+
+        assert_eq!(String::from(table.trim_matches('\n')), Table::build(items));
     }
 }
