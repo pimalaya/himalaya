0bf57aaabbfc2ed9046bf0f6d488c3619e1f28a7
TornaxO7 <tornax07@gmail.com>
2021-05-21 15:54:08 +0200

    Initial commit
    Created the `src/tui` directory with
    
    ```bash
    cargo new --lib tui
    ```

---
+++ src/tui/target/debug/deps/tui-45d2c35c1e5f226c.d
@@ -0,0 +1,5 @@
+/windows/Programming/pull_requests/himalaya/src/tui/target/debug/deps/tui-45d2c35c1e5f226c.rmeta: src/lib.rs
+
+/windows/Programming/pull_requests/himalaya/src/tui/target/debug/deps/tui-45d2c35c1e5f226c.d: src/lib.rs
+
+src/lib.rs:
