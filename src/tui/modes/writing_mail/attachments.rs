836fef15b63089d1118b33a0b1faf58ca8576c13
TornaxO7 <tornax07@gmail.com>
2021-06-03 02:18:38 +0200

    Attachments
    Just a little backup-comment before merging with master.

--- src/tui/modes/writing_mail/attachments.rs
+++ src/tui/modes/writing_mail/attachments.rs
@@ -1,12 +1,16 @@
 use crate::config::tui::BlockDataConfig;
 use crate::tui::modes::block_data::BlockData;
-use tui_rs::widgets::{Block, Row, Table};
+use crate::tui::modes::table_state_wrapper::TableStateWrapper;
+
+use tui_rs::widgets::{Block, Row, Table, TableState};
 
 // ============
 // Structs
 // ============
 pub struct Attachments {
     block_data: BlockData,
+
+    pub state: TableStateWrapper,
 }
 
 impl Attachments {
@@ -16,14 +20,21 @@ impl Attachments {
                 String::from("Attachments"),
                 block_config,
             ),
+            state: TableStateWrapper::new(),
         }
     }
 
     pub fn widget(&self) -> Table<'static> {
-
         let block = Block::from(self.block_data.clone());
 
-        Table::new(vec![Row::new(vec!["Yeet"])])
-            .block(block)
+        Table::new(vec![Row::new(vec!["Yeet"])]).block(block)
+    }
+
+    pub fn get_state(&mut self) -> &mut TableState {
+        &mut self.state.state
+    }
+
+    pub fn move_selection(&mut self, offset: i32) {
+        self.state.move_selection(offset);
     }
 }
