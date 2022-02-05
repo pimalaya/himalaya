f939b6042ceb1400dc4ae9a74eea47673606d51f
Clément DOUIN <clement.douin@posteo.net>
2022-02-05 12:54:04 +0100

    improve main comments

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,8 @@ fn create_app<'a>() -> clap::App<'a, 'a> {
 
 #[allow(clippy::single_match)]
 fn main() -> Result<()> {
-    // Init env logger
-    env_logger::init_from_env(
-        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "off"),
-    );
+    let default_env_filter = env_logger::DEFAULT_FILTER_ENV;
+    env_logger::init_from_env(env_logger::Env::default().filter_or(default_env_filter, "off"));
 
     // Check mailto command BEFORE app initialization.
     let raw_args: Vec<String> = env::args().collect();
