f3d3053409d2b8f070bedba923adc67ee43a0e61
Clément DOUIN <clement.douin@posteo.net>
2022-02-18 20:17:09 +0100

    rename mbox entities

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,14 @@ pub mod mbox {
     pub mod mbox_arg;
     pub mod mbox_handler;
 
-    pub use attr_entity::*;
-    pub mod attr_entity;
+    pub use mbox_attr::*;
+    pub mod mbox_attr;
 
     pub use attrs_entity::*;
     pub mod attrs_entity;
 
-    pub use mbox_entity::*;
-    pub mod mbox_entity;
+    pub use mbox::*;
+    pub mod mbox;
 
     pub use mboxes_entity::*;
     pub mod mboxes_entity;
--- src/mbox/mbox_attr.rs
+++ src/mbox/mbox_attr.rs
@@ -1,13 +1,47 @@
-//! Mailbox attribute entity module.
+//! Mailbox attribute module.
 //!
-//! This module contains the definition of the mailbox attribute and its traits implementations.
+//! This module contains the definition of the mailbox attribute and
+//! its traits implementations.
 
 pub use imap::types::NameAttribute as AttrRemote;
 use serde::Serialize;
 use std::{
     borrow::Cow,
     fmt::{self, Display},
+    ops::Deref,
 };
+
+/// Represents the attributes of the mailbox.
+#[derive(Debug, Default, PartialEq, Eq, Serialize)]
+pub struct Attrs<'a>(Vec<Attr<'a>>);
+
+/// Converts a vector of `imap::types::NameAttribute` into attributes.
+impl<'a> From<Vec<AttrRemote<'a>>> for Attrs<'a> {
+    fn from(attrs: Vec<AttrRemote<'a>>) -> Self {
+        Self(attrs.into_iter().map(Attr::from).collect())
+    }
+}
+
+/// Derefs the attributes to its inner hashset.
+impl<'a> Deref for Attrs<'a> {
+    type Target = Vec<Attr<'a>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Makes the attributes displayable.
+impl<'a> Display for Attrs<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut glue = "";
+        for attr in self.iter() {
+            write!(f, "{}{}", glue, attr)?;
+            glue = ", ";
+        }
+        Ok(())
+    }
+}
 
 /// Wraps an `imap::types::NameAttribute`.
 /// See https://serde.rs/remote-derive.html.
@@ -49,6 +83,28 @@ impl<'a> From<AttrRemote<'a>> for Attr<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_should_display_attrs() {
+        macro_rules! attrs_from {
+            ($($attr:expr),*) => {
+                Attrs::from(vec![$($attr,)*]).to_string()
+            };
+        }
+
+        let empty_attr = attrs_from![];
+        let single_attr = attrs_from![AttrRemote::NoInferiors];
+        let multiple_attrs = attrs_from![
+            AttrRemote::Custom("AttrCustom".into()),
+            AttrRemote::NoInferiors
+        ];
+
+        assert_eq!("", empty_attr);
+        assert_eq!("NoInferiors", single_attr);
+        assert!(multiple_attrs.contains("NoInferiors"));
+        assert!(multiple_attrs.contains("AttrCustom"));
+        assert!(multiple_attrs.contains(","));
+    }
 
     #[test]
     fn it_should_display_attr() {
