dea7fd562e8bf2cf321a4e1e3474a0f80ec989a5
TornaxO7 <tornax07@gmail.com>
2022-02-27 23:54:18 +0100

    adding first interaction with wizard

--- src/config/wizard.rs
+++ src/config/wizard.rs
@@ -1,3 +1,33 @@
+use anyhow::{Result, anyhow};
+
+use dialoguer::Confirm;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Wizard {
+pub struct Wizard;
+
+impl Wizard {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn start() -> Result<String> {
+        let wizard = Wizard::new();
+
+        if !wizard.is_allowed_to_create_config()? {
+            println!("Easter egg");
+        }
+
+        Ok("yeet".to_string())
+    }
+
+    fn is_allowed_to_create_config(&self) -> Result<bool> {
+
+        println!("Oh! Looks like that you don't have a config file!");
+
+        Confirm::new()
+            .with_prompt("Should we (you and me, the config wizard) create the config file together? :)")
+            .default(true)
+            .interact_opt()?
+            .ok_or(anyhow!("Cancelled wizard."))
+    }
 }
