219064dac1450cc957df7e506024f5a7680ea06e
TornaxO7 <tornax07@gmail.com>
2022-02-27 23:17:09 +0100

    removing old wizard files and adding dialoguer to the dependencies

---
+++ src/config/wizard.rs
@@ -0,0 +1,3 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Wizard {
+}
--- src/config/wizard/wizard_error.rs
+++
@@ -1,5 +0,0 @@
-#[derive(thiserror::Error, Debug, Clone, PartialEq, Eq)]
-pub enum WizardError {
-    #[error("User denied wizard :(")]
-    Unallowed,
-}
