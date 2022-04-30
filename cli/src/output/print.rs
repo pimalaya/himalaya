303bd1d2a7383ad336d394214c545273a02731a4
Robert Günzler <r@gnzler.io>
2022-04-30 15:14:31 +0200

    Always reset colors settings on the output stream after writing
    This is according to:
    https://docs.rs/termcolor/1.1.2/termcolor/#example-using-standardstream
    Not resetting the color settings on the stream will leak the style to
    the shell otherwise.
    This can be observed when listing mailboxes prior to this patch.
    
    Signed-off-by: Robert Günzler <r@gnzler.io>

--- cli/src/output/print.rs
+++ cli/src/output/print.rs
@@ -8,12 +8,14 @@ pub trait Print {
 
 impl Print for &str {
     fn print(&self, writer: &mut dyn WriteColor) -> Result<()> {
-        writeln!(writer, "{}", self).context("cannot write string to writer")
+        writeln!(writer, "{}", self).context("cannot write string to writer")?;
+        Ok(writer.reset()?)
     }
 }
 
 impl Print for String {
     fn print(&self, writer: &mut dyn WriteColor) -> Result<()> {
-        self.as_str().print(writer)
+        self.as_str().print(writer)?;
+        Ok(writer.reset()?)
     }
 }
