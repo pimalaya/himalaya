aa7a854672ba280db16d5530f6c3870171e161de
kmaasrud <km@aasrud.com>
2023-01-10 17:11:18 +0100

    feat: add SMTP part of wizard

--- src/config/wizard.rs
+++ src/config/wizard.rs
@@ -3,6 +3,7 @@ use crate::account::{DeserializedAccountConfig, DeserializedBaseAccountConfig};
 use anyhow::{anyhow, Result};
 use dialoguer::{theme::ColorfulTheme, Confirm, Input, Select, Validator};
 use email_address::EmailAddress;
+use himalaya_lib::{EmailSender, SendmailConfig, SmtpConfig};
 use log::trace;
 use once_cell::sync::Lazy;
 
@@ -14,6 +15,10 @@ const BACKENDS: &[&str] = &[
     #[cfg(feature = "notmuch-backend")]
     "Notmuch",
 ];
+
+const SENDERS: &[&str] = &["SMTP", "Sendmail"];
+
+const SECURITY_PROTOCOLS: &[&str] = &["SSL/TLS", "STARTTLS", "None"];
 
 // A wizard should have pretty colors 💅
 static THEME: Lazy<ColorfulTheme> = Lazy::new(ColorfulTheme::default);
@@ -100,6 +105,19 @@ pub(crate) fn wizard() -> Result<DeserializedConfig> {
 }
 
 fn configure_account() -> Result<Option<DeserializedAccountConfig>> {
+    let mut base = configure_base()?;
+    let sender = Select::with_theme(&*THEME)
+        .with_prompt("Which sender would you like use with your account?")
+        .items(SENDERS)
+        .default(0)
+        .interact_opt()?;
+
+    base.email_sender = match sender {
+        Some(idx) if SENDERS[idx] == "SMTP" => configure_smtp(&base),
+        Some(idx) if SENDERS[idx] == "Sendmail" => configure_sendmail(),
+        _ => return Ok(None),
+    }?;
+
     let backend = Select::with_theme(&*THEME)
         .with_prompt("Which backend would you like to configure your account for?")
         .items(BACKENDS)
@@ -108,31 +126,80 @@ fn configure_account() -> Result<Option<DeserializedAccountConfig>> {
 
     match backend {
         #[cfg(feature = "imap-backend")]
-        Some(idx) if BACKENDS[idx] == "IMAP" => Ok(Some(configure_imap()?)),
+        Some(idx) if BACKENDS[idx] == "IMAP" => Ok(Some(configure_imap(base)?)),
         #[cfg(feature = "maildir-backend")]
-        Some(idx) if BACKENDS[idx] == "Maildir" => Ok(Some(configure_maildir()?)),
+        Some(idx) if BACKENDS[idx] == "Maildir" => Ok(Some(configure_maildir(base)?)),
         #[cfg(feature = "notmuch-backend")]
-        Some(idx) if BACKENDS[idx] == "Notmuch" => Ok(Some(configure_notmuch()?)),
+        Some(idx) if BACKENDS[idx] == "Notmuch" => Ok(Some(configure_notmuch(base)?)),
         _ => Ok(None),
     }
 }
 
+fn configure_smtp(base: &DeserializedBaseAccountConfig) -> Result<EmailSender> {
+    let mut smtp_config = SmtpConfig::default();
+
+    smtp_config.host = Input::with_theme(&*THEME)
+        .with_prompt("Enter the SMTP host: ")
+        .default(format!("smtp.{}", base.email.rsplit_once('@').unwrap().1))
+        .interact()?;
+
+    smtp_config.port = Input::with_theme(&*THEME)
+        .with_prompt("Enter the SMTP port:")
+        .validate_with(|input: &String| input.parse::<u16>().map(|_| ()))
+        .default(465.to_string())
+        .interact()
+        .map(|input| input.parse::<u16>().unwrap())?;
+
+    smtp_config.login = Input::with_theme(&*THEME)
+        .with_prompt("Enter your SMTP login:")
+        .default(base.email.clone())
+        .interact()?;
+
+    smtp_config.passwd_cmd = Input::with_theme(&*THEME)
+        .with_prompt("What shell command should we run to get your password?")
+        .default(format!("pass show {}", &base.email))
+        .interact()?;
+
+    match Select::with_theme(&*THEME)
+        .with_prompt("Which security protocol do you want to use?")
+        .items(SECURITY_PROTOCOLS)
+        .default(0)
+        .interact_opt()?
+    {
+        Some(idx) if SECURITY_PROTOCOLS[idx] == "SSL/TLS" => smtp_config.ssl = Some(true),
+        Some(idx) if SECURITY_PROTOCOLS[idx] == "STARTTLS" => smtp_config.starttls = Some(true),
+        _ => {}
+    };
+
+    Ok(EmailSender::Smtp(smtp_config))
+}
+
+fn configure_sendmail() -> Result<EmailSender> {
+    Ok(EmailSender::Sendmail(SendmailConfig {
+        cmd: Input::with_theme(&*THEME)
+            .with_prompt("Enter an external command to send a mail: ")
+            .default("/usr/bin/msmtp".to_owned())
+            .interact()?,
+    }))
+}
+
 #[cfg(feature = "imap-backend")]
-fn configure_imap() -> Result<DeserializedAccountConfig> {
+fn configure_imap(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
     use crate::account::DeserializedImapAccountConfig;
     use himalaya_lib::ImapConfig;
 
-    let base = configure_base()?;
     let mut backend = ImapConfig::default();
 
     // TODO: Validate by checking as valid URI
     backend.host = Input::with_theme(&*THEME)
         .with_prompt("Enter the IMAP host:")
+        .default(format!("imap.{}", base.email.rsplit_once('@').unwrap().1))
         .interact()?;
 
     backend.port = Input::with_theme(&*THEME)
         .with_prompt("Enter the IMAP port:")
         .validate_with(|input: &String| input.parse::<u16>().map(|_| ()))
+        .default(993.to_string())
         .interact()
         .map(|input| input.parse::<u16>().unwrap())?;
 
@@ -152,8 +219,8 @@ fn configure_imap() -> Result<DeserializedAccountConfig> {
         .default(0)
         .interact_opt()?
     {
-        Some(0) => backend.ssl = Some(true),
-        Some(1) => backend.starttls = Some(true),
+        Some(idx) if SECURITY_PROTOCOLS[idx] == "SSL/TLS" => backend.ssl = Some(true),
+        Some(idx) if SECURITY_PROTOCOLS[idx] == "STARTTLS" => backend.starttls = Some(true),
         _ => {}
     };
 
@@ -163,12 +230,12 @@ fn configure_imap() -> Result<DeserializedAccountConfig> {
 }
 
 #[cfg(feature = "maildir-backend")]
-fn configure_maildir() -> Result<DeserializedAccountConfig> {
+fn configure_maildir(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
     use crate::account::DeserializedMaildirAccountConfig;
     use himalaya_lib::MaildirConfig;
 
-    let base = configure_base()?;
     let backend = MaildirConfig::default();
+    // TODO
 
     Ok(DeserializedAccountConfig::Maildir(
         DeserializedMaildirAccountConfig { base, backend },
@@ -176,12 +243,12 @@ fn configure_maildir() -> Result<DeserializedAccountConfig> {
 }
 
 #[cfg(feature = "notmuch-backend")]
-fn configure_notmuch() -> Result<DeserializedAccountConfig> {
+fn configure_notmuch(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
     use crate::account::DeserializedNotmuchAccountConfig;
     use himalaya_lib::NotmuchConfig;
 
-    let base = configure_base()?;
     let backend = NotmuchConfig::default();
+    // TODO
 
     Ok(DeserializedAccountConfig::Notmuch(
         DeserializedNotmuchAccountConfig { base, backend },
