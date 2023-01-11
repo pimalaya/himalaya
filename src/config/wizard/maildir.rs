3c67dc553cc004bf29d6f4b2b0576fd8eccc3f03
kmaasrud <km@aasrud.com>
2023-01-11 17:52:50 +0100

    feat: add maildir part of wizard

--- src/config/wizard/maildir.rs
+++ src/config/wizard/maildir.rs
@@ -1,15 +1,31 @@
+use super::THEME;
 use crate::account::{
     DeserializedAccountConfig, DeserializedBaseAccountConfig, DeserializedMaildirAccountConfig,
 };
 use anyhow::Result;
+use dialoguer::Input;
+use dirs::home_dir;
 use himalaya_lib::MaildirConfig;
 
 #[cfg(feature = "maildir-backend")]
 pub(crate) fn configure(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
-    let backend = MaildirConfig::default();
-    // TODO
+    let input = if let Some(home) = home_dir() {
+        Input::with_theme(&*THEME)
+            .default(home.join("Mail").display().to_string())
+            .with_prompt("Enter the path to your maildir")
+            .interact_text()?
+    } else {
+        Input::with_theme(&*THEME)
+            .with_prompt("Enter the path to your maildir")
+            .interact_text()?
+    };
 
     Ok(DeserializedAccountConfig::Maildir(
-        DeserializedMaildirAccountConfig { base, backend },
+        DeserializedMaildirAccountConfig {
+            base,
+            backend: MaildirConfig {
+                root_dir: input.into(),
+            },
+        },
     ))
 }
