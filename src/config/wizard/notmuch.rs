9469daa47b98ed9d0b7ab89cbdd3073eef55a61a
kmaasrud <km@aasrud.com>
2023-01-18 11:27:14 +0100

    feat: add notmuch part of wizard

--- src/config/wizard/notmuch.rs
+++ src/config/wizard/notmuch.rs
@@ -1,10 +1,29 @@
+use super::THEME;
+use crate::account::{
+    DeserializedAccountConfig, DeserializedBaseAccountConfig, DeserializedNotmuchAccountConfig,
+};
+use anyhow::Result;
+use dialoguer::Input;
+use himalaya_lib::NotmuchConfig;
+use std::path::PathBuf;
+
 #[cfg(feature = "notmuch-backend")]
 pub(crate) fn configure(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
-    use crate::account::DeserializedNotmuchAccountConfig;
-    use himalaya_lib::NotmuchConfig;
 
-    let backend = NotmuchConfig::default();
-    // TODO
+    let db_path: PathBuf = match std::process::Command::new("notmuch")
+        .args(["config", "get", "database.path"])
+        .output()
+    {
+        Ok(output) => PathBuf::from(String::from_utf8(output.stdout)?),
+        _ => {
+            let input: String = Input::with_theme(&*THEME)
+                .with_prompt("Could not find a notmuch database. Enter path manually:")
+                .interact_text()?;
+            input.into()
+        }
+    };
+
+    let backend = NotmuchConfig { db_path };
 
     Ok(DeserializedAccountConfig::Notmuch(
         DeserializedNotmuchAccountConfig { base, backend },
