ddb12e0159ad275534f0597c90756c143eaee0c7
Clément DOUIN <clement.douin@posteo.net>
2023-01-27 15:59:46 +0100

    fix notmuch wizard

--- src/config/wizard/notmuch.rs
+++ src/config/wizard/notmuch.rs
@@ -4,15 +4,11 @@ use crate::account::{
 };
 use anyhow::Result;
 use dialoguer::Input;
-use himalaya_lib::{
-    notmuch::{Database, DatabaseMode},
-    NotmuchConfig,
-};
-use std::path::PathBuf;
+use himalaya_lib::{NotmuchBackend, NotmuchConfig};
 
 pub(crate) fn configure(base: DeserializedBaseAccountConfig) -> Result<DeserializedAccountConfig> {
-    let db_path = match Database::open_with_config(None, DatabaseMode::ReadOnly, None, None) {
-        Ok(db) => db.path(),
+    let db_path = match NotmuchBackend::get_default_db_path() {
+        Ok(db) => db,
         _ => {
             let input: String = Input::with_theme(&*THEME)
                 .with_prompt("Could not find a notmuch database. Enter path manually:")
