41f5aa4fe463f56874211ade14205ecf4927ab01
Clément DOUIN <clement.douin@posteo.net>
2024-11-25 22:42:17 +0100

    try missing -lunwind

--- package.nix
+++ package.nix
@@ -32,6 +32,7 @@ rustPlatform.buildRustPackage rec {
   NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isWindows && stdenv.hostPlatform.isi686) [
     "-L${windows.mcfgthreads}/lib"
     "-lmcfgthread"
+    "-lunwind"
   ];
 
   # unit tests only
