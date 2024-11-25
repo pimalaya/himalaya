5fbd407b4493f63ec2ecc5b9fd8dc206e23189f3
Clément DOUIN <clement.douin@posteo.net>
2024-11-25 16:06:48 +0100

    fix macos trial 4

--- package.nix
+++ package.nix
@@ -28,8 +28,8 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-YS8IamapvmdrOPptQh2Ef9Yold0IK1XIeGs0kDIQ5b8=";
 
-  NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) [
-    "-F${darwin.apple_sdk_11_0.frameworks.Security}/Library/Frameworks -framework Security"
+  NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isDarwin) [
+    "-L${darwin.libiconv}"
   ];
 
   # unit tests only
