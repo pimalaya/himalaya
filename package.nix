d1c5d0397e39a9326aabb3d51ada3e432e8b7a1c
Clément DOUIN <clement.douin@posteo.net>
2024-11-26 09:49:32 +0100

    remove NIX_LDFLAGS

--- package.nix
+++ package.nix
@@ -31,7 +31,7 @@ rustPlatform.buildRustPackage rec {
 
   NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isWindows && stdenv.hostPlatform.isi686) [
     # "-L${windows.mcfgthreads}/lib"
-    "-L${windows.mingwrt}/lib"
+    # "-L${windows.mingwrt}/lib"
     #"-L${windows.w32api}/lib"
     #"-lmcfgthread"
     #"-lunwind"
