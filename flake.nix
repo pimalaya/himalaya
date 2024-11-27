74fcc0d44fe3c174df4292420536ff9b52d84663
Clément DOUIN <clement.douin@posteo.net>
2024-11-27 10:10:20 +0100

    fix target name in CI

--- flake.nix
+++ flake.nix
@@ -15,11 +15,15 @@
       inherit (nixpkgs) lib;
 
       crossSystems = {
-        aarch64-apple = [ ];
+        aarch64-apple = [
+          "aarch64-apple-darwin"
+        ];
         aarch64-linux = [
           "aarch64-unknown-linux-musl"
         ];
-        x86_64-apple = [ ];
+        x86_64-apple = [
+          "x86_64-apple-darwin"
+        ];
         x86_64-linux = [
           "aarch64-unknown-linux-musl"
           "armv6l-unknown-linux-musleabihf"
