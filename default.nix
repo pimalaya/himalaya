976b4c0650a96efe7f568a72d5051158a5072aa5
Clément DOUIN <clement.douin@posteo.net>
2026-01-12 16:11:22 +0100

    build(nix): use nixpkgs derivation instead of local package.nix
    Refs: #558

--- default.nix
+++ default.nix
@@ -6,23 +6,21 @@
 pimalaya.mkDefault (
   {
     src = ./.;
-    version = "1.0.0";
+    version = "1.1.0";
     mkPackage = (
       {
         lib,
         pkgs,
-        rustPlatform,
         defaultFeatures,
         features,
+        ...
       }:
-      pkgs.callPackage ./package.nix {
-        inherit lib rustPlatform;
-        apple-sdk = pkgs.apple-sdk;
+      pkgs.himalaya.overrideAttrs (_: {
         installShellCompletions = false;
         installManPages = false;
         buildNoDefaultFeatures = !defaultFeatures;
         buildFeatures = lib.splitString "," features;
-      }
+      })
     );
   }
   // removeAttrs args [ "pimalaya" ]
