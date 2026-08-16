19f3772afb755654cc1c94ea3c7fec1d3554f27c
Clément DOUIN <clement.douin@posteo.net>
2026-08-16 21:13:14 +0200

    nix: make derivation closer to the soon-to-land nixpkgs' one

--- default.nix
+++ default.nix
@@ -4,52 +4,59 @@
   ...
 }@args:
 
+let
+  himalaya = import ./default.nix (
+    removeAttrs args [
+      "crossPkgs"
+      "isStatic"
+      "target"
+    ]
+  );
+
+in
 pimalaya.mkDefault (
   {
     src = ./.;
-    version = "2.0.0";
+    version = "2.1.0";
     mkPackage = (
       {
         lib,
         pkgs,
-        buildPackages,
         rustPlatform,
         defaultFeatures,
         features,
+        buildPackages,
       }:
+
       (pkgs.callPackage "${nixpkgs}/pkgs/by-name/hi/himalaya/package.nix" {
         inherit lib rustPlatform;
+        # the nixpkgs derivation runs the binary it just built, which needs
+        # a native one when cross compiling
+        buildPackages = buildPackages // {
+          inherit himalaya;
+        };
+        installShellCompletions = false;
+        installManPages = false;
         buildNoDefaultFeatures = !defaultFeatures;
         buildFeatures = lib.splitString "," features;
       })
-      # HACK: needed until new derivation available on nixpkgs's
-      # master branch
+      # HACK: needed until the v2.1.0 derivation lands on nixpkgs's master
       .overrideAttrs
         {
           postInstall =
             let
               inherit (pkgs) stdenv;
-              emulator = stdenv.hostPlatform.emulator buildPackages;
-              exe = stdenv.hostPlatform.extensions.executable;
+              exe =
+                if stdenv.buildPlatform.canExecute stdenv.hostPlatform then
+                  "$out/bin/himalaya"
+                else
+                  lib.getExe himalaya;
             in
-            lib.optionalString (lib.hasInfix "wine" emulator) ''
-              export WINEPREFIX="''${WINEPREFIX:-$(mktemp -d)}"
-              mkdir -p $WINEPREFIX
             ''
-            + ''
               mkdir -p $out/share/{completions,man,schemas}
-              ${emulator} "$out"/bin/himalaya${exe} completion -d "$out"/share/completions bash elvish fish powershell zsh
-              ${emulator} "$out"/bin/himalaya${exe} manual "$out"/share/man
-              ${emulator} "$out"/bin/himalaya${exe} json-schema "$out"/share/schemas
-            ''
-            + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-              installManPage "$out"/share/man/*
-            ''
-            + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-              installShellCompletion --cmd himalaya \
-                --bash "$out"/share/completions/himalaya.bash \
-                --fish "$out"/share/completions/himalaya.fish \
-                --zsh "$out"/share/completions/_himalaya
+              ${exe} completion -d "$out"/share/completions bash elvish fish powershell zsh
+              ${exe} manual "$out"/share/man
+              ${exe} json-schema "$out"/share/schemas
             '';
         }
     );
